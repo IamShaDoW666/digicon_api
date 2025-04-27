ddd4dbbecfd49ea0fd436d5090ebf3b65f5e0d3b
nibhapr <nibhapr@gmail.com>
2025-04-27 18:11:35 +0530

    mysql database link updated

--- src/app/admin/batches/page.tsx
+++ src/app/admin/batches/page.tsx
@@ -14,16 +14,18 @@ const batchesList = async () => {
 
   return (
     <>
+ 
       <SiteHeader title="Batches" />
       <div className="flex flex-1 flex-col">
         <div className="@container/main flex flex-1 flex-col gap-2">
           <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+
             <BatchDataTable data={allBatches} />
           </div>
         </div>
       </div>
     </>
-  );
-};
+   );
+ };
 
 export default batchesList;
